8d35d2657766d8c26b4cfccb394efee398509466
Olivier Wietrich <olivier.wietrich@gmail.com>
2025-05-12 14:59:22 -0400

    chore: change status hook to return error that can be thrown

--- packages/sensai/src/lib/hooks/status.ts
+++ packages/sensai/src/lib/hooks/status.ts
@@ -1,8 +1,10 @@
 import context from "@/src/lib/server/context";
 import { Context } from "@/src/types";
+import ServerError from "@/src/lib/server/error";
 
-export default (code: number = 200, errorMessage?: string): void => {
+export default (code: number = 200, errorMessage?: string): ServerError => {
   const { status } = context.getStore() as Context;
   status.code = code;
   status.errorMessage = errorMessage;
+  return new ServerError(code, errorMessage);
 };
